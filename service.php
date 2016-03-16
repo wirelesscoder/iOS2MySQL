5be5c6e357e8133fa4a38a4b35ebefc6b61dd16d
wirelesscoder <wirelesscoder@users.noreply.github.com>
2016-03-16 14:54:15 +0100

    service.php
    add service.php

--- service.php
+++ service.php
@@ -3,11 +3,17 @@
 //Connection to MySQL-database
 $con=mysqli_connect("dbHost","userName","passWord",dbName");
 
-//check connection error
-if (mysqli_connect_errorno())
+//if Connection failed print error message
+if (mysqli_connect_errorno()) 
+{
+  echo "Connection to database failled with error:" . mysqli_connect_error()
+}
 
 //SQL statement 
 
 
 
 //close connection
+
+?>
+
