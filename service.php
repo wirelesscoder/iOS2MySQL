0dcbec4a51e87139db8790b03847c87dbed923f2
wirelesscoder <wirelesscoder@users.noreply.github.com>
2016-03-16 13:47:32 +0100

    Create service.php

---
+++ service.php
@@ -0,0 +1,13 @@
+<?php
+
+//Connection to MySQL-database
+$con=mysqli_connect("dbHost","userName","passWord",dbName");
+
+//check connection error
+if (mysqli_connect_errorno())
+
+//SQL statement 
+
+
+
+//close connection
