e492a654dd5acf4d6c20995977c8a472263f35a7
wirelesscoder <wirelesscoder@users.noreply.github.com>
2016-03-17 12:35:32 +0100

    Update webservice.php

--- php/webservice.php
+++ php/webservice.php
@@ -2,44 +2,25 @@
 
   echo "webservice";
   
-  //db variables
+  //db constants
   
   define('DB_USER', 'web235_1');
   define('DB_PASSWORD','Salome45');
   define('DB_NAME','web235_db1');
   define('DB_HOST', 'localhost');
 
-
   //SQL statement 
-  //$sql = "SELECT * FROM tbl_user";
+  $sql = "SELECT * FROM tbl_user";
   
-  
-  
-
-
-
   //array to store result
-  //$arrayResult = array();
-  //$arrayTempResult = array();
+  $arrayResult = array();
+  $arrayTempResult = array();
   
   //Connection to MySQL-database
-  //$con=mysqli_connect($dbHost, $dbUserName, $dbPassWord, $dbName);
-
-  //$con=mysqli_connect("localhost","web235_1","Salome45","web235_db1");
-
   $dbcon = @mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) OR die('Could not connect to MySQL ' . mysqli_connect_error());
-  
 
-
-  
-  //if Connection failed print error message 
-  //if (mysqli_connect_errorno()) 
-  //  {
-  //    echo "Connection to database failled with error";
-  //  }
-  
   //store result into array 
-  /*if ($result = mysqli_query($con, $sql))
+  if ($result = mysqli_query($dbcon, $sql))
     {
         while ($row = $result->fetch_object())
         {
@@ -50,9 +31,9 @@
         //output result in JSON format
         echo json_encode($arrayResult);
     }
-  */
+  
   //close connection
-  //mysqli_close($dbcon);
+  mysqli_close($dbcon);
 
 ?>
 
